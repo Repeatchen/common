aecbd1bade050588856dd6dca54fb7bc44078152
陈方玉 <123456>
2019-12-04 14:15:49 +0800

    注解

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# cd ~/Desktop/project/new-manager/manager
 echo '当前文件状态'
 git status
 git add -A
@@ -6,12 +7,17 @@ git status
 echo "输入commit注解"
 read msg
 if [ -n "$msg" ]; then
-git commit -m"${msg}"
-git pull
-git status
-echo "完成add、commit、pull，别忘了push"
+    git commit -m"${msg}"
+    git pull
+    read y
+    echo '是否要push [Y/N]?'
+    if [ $y == 'y' ]; then
+        git push
+    else
+        echo "完成add、commit、pull，别忘了push"
+    fi
 else
-echo "请添加注释再来一遍"
+    echo "请添加注释再来一遍"
 fi
 
 
